3f47f589a4f410ec0f2ac306fd15213e920ac0d4
Matthias <maeddes@gmail.com>
2020-02-06 10:01:38 +0100

    OOP 2020

--- src/main/java/de/maeddes/SimpleWeb/SimpleWebApplication.java
+++ src/main/java/de/maeddes/SimpleWeb/SimpleWebApplication.java
@@ -30,14 +30,14 @@ public class SimpleWebApplication {
 	@GetMapping("/hello")
 	String hello(){
 
-		return getInstanceId()+" Hello, HfT 2020! ";
+		return getInstanceId()+" Hello, OOP 2020! ";
 
 	}
 
 	@GetMapping("/fail")
 	String fail() {
 
-		//System.exit(1);
+		System.exit(1);
 		return "fixed!";
 	}
 
